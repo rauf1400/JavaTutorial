6bc8b0e9235b9cb494589ef89d1dc904244a39b3
Android Developer <and.dev1399@gmail.com>
2021-01-02 01:41:51 +0330

    commit

---
+++ src/Navigator.java
@@ -0,0 +1,12 @@
+/**
+ * This Class Navigate us  to the Target Classes
+ */
+public class Navigator {
+    /**
+     * Let's goooooo
+     * @param args
+     */
+    public static void  main(String [] args){
+        System.out.println("Navigator");
+    }
+}
